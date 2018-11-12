267473a5b65af1a255e1fded07bc97d93963e9eb
Brian Welch <myself@brianwelch.se>
2018-11-12 22:00:50 +0100

    'return' changed to 'return_date' in bookings migration

--- db/migrate/20181112194547_create_bookings.rb
+++ db/migrate/20181112194547_create_bookings.rb
@@ -4,7 +4,7 @@ class CreateBookings < ActiveRecord::Migration[5.2]
       t.references :artifact, foreign_key: true
       t.references :user, foreign_key: true
       t.date :check_out_date
-      t.date :return
+      t.date :return_date
 
       t.timestamps
     end
