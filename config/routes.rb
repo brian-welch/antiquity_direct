a30ffc5cf66030f61915af2d2f250d112871cfff
Brian Welch <myself@brianwelch.se>
2018-11-12 16:23:28 +0100

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'design', to: 'pages#design'
+  resources :artifacts, only: [:index, :show, :new, :create]
+  post 'bookings', to: 'bookings#create'
+  get 'artifacts/:id/contact', to: 'contacts#new'
+  post 'contacts', to: 'contacts#create'
+  get 'dashboard', to: 'dashboards#index'
 end
