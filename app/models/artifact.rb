fc9ebe2ec3a50d9d9a5d560b06a024aab7868bc7
Brian Welch <myself@brianwelch.se>
2018-11-12 21:49:47 +0100

    some errant lines removed form artifact model

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -12,17 +12,3 @@ class Artifact < ApplicationRecord
   validates :artifact_type_id, presence: true
   validates :condition_id, presence: true
 end
-
-
-    t.string "name"
-    t.text "description"
-    t.float "price"
-    t.integer "discount"
-    t.string "photo"
-    t.boolean "free_shipping"
-    t.bigint "cultural_origin_id"
-    t.bigint "time_period_id"
-    t.bigint "artifact_type_id"
-    t.bigint "condition_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
