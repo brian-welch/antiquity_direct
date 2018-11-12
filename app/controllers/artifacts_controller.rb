dc32ab1879af50a7b58e4d6e26841c8e8a199f1f
Beltran Sainz de Vicuna Nowack <gorthor@gmail.com>
2018-11-12 18:03:22 +0100

    seeded db, started index - show (view) WIP

--- app/controllers/artifacts_controller.rb
+++ app/controllers/artifacts_controller.rb
@@ -1,8 +1,13 @@
 class ArtifactsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:home, :index, :show]
   def index
+    # write code (display list of all artifacts from db -- Assign to instance variable)
+    @artifacts = Artifact.all
   end
 
   def show
+    # write code to get a specific artifact from db (assign to instance variable)
+    @artifact = Artifact.find(params[:id])
   end
 
   def new
