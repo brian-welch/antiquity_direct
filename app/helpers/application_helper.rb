de393206c79d343d2ec042049272433a38d521df
Brian Welch <myself@brianwelch.se>
2018-11-14 09:03:42 +0100

    moved the avatar image logic to a help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def user_photo
+    if current_user.photo.nil?
+      return "https://ui-avatars.com/api/?name=#{me.first_name}+#{me.last_name}&background=0D8ABC&color=eaec96"
+    else
+      return current_user.photo
+    end
+  end
 end
