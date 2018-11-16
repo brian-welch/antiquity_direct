72d2e5397b1418176141700663cb26bca3b9d7e7
Brian Welch <myself@brianwelch.se>
2018-11-16 09:02:32 +0100

    dahsboard>my bookings table now with responsive color

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,21 @@
 //= require rails-ujs
 //= require_tree .
+
+
+
+// Change my booking button color if booking confirmed
+myBookingStatusArray = document.querySelectorAll('.my-booking-status');
+myBookingStatusArray.forEach((element) => {
+  if (element.innerText === "confirmed"){
+    console.log('fire!')
+    element.classList.add('my-confirmed-booking');
+  };
+});
+
+
+myBookingStatusArray = document.querySelectorAll('.my-booking-status');
+myBookingStatusArray.forEach((element) => {
+  if (element.innerText === "declined"){
+    element.classList.add('my-declined-booking');
+  };
+});
