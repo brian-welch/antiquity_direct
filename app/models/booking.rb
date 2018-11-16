504861ea924eb4e05a21401fb5bfb49defdc045c
Brian Welch <myself@brianwelch.se>
2018-11-16 07:06:15 +0100

    dishboard shows again - validation logic problem persists

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,12 +5,20 @@ class Booking < ApplicationRecord
   belongs_to :user
 
   validate :valid_dates
+  validate :self_booking_check
 
   def valid_dates
     if check_out_date.present? && check_out_date < Date.today
-      errors.add(:check_out_date, " should not be in the past!")
+      errors.add(:base, "You cannot travel back in time!")
     elsif return_date.present? && return_date <= (Date.today + 7)
       errors.add(:base, "You must commit to rent an artifact for at least 7 days")
+    end
+  end
+
+  def self_booking_check
+    # raise
+    if user.artifacts.ids.include?(:artifact_id)
+      errors.add(:base, "You cannot book your own artifact.")
     end
   end
 end
